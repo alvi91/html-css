8fbc140d6750081f41baadcc8d8a4c7e7016cd5a
alvi91 <alvi.zadid@gmail.com>
2023-08-04 05:16:30 +0600

    working on javascript

---
+++ javaScript/Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,29 @@
+/*let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log("23");
+
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let Jonas_Matilda = "JM";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "Proggramer";
+let Job2 = "Teacher";
+
+console.log(myFirstJob);*/
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
