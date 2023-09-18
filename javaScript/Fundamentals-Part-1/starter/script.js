b81d31c1b0f3179c13d37918b64bace2f42345e1
alvi91 <alvi.zadid@gmail.com>
2023-09-19 01:08:56 +0600

    css grid

--- javaScript/Fundamentals-Part-1/starter/script.js
+++ javaScript/Fundamentals-Part-1/starter/script.js
@@ -20,10 +20,95 @@ let Job2 = "Teacher";
 
 console.log(myFirstJob);*/
 
-let javaScriptIsFun = true;
+/*let javaScriptIsFun = true;
 console.log(javaScriptIsFun);
 
-console.log(typeof true);
+//console.log(typeof true);
 console.log(typeof javaScriptIsFun);
-console.log(typeof 23);
-console.log(typeof "Jonas");
+//console.log(typeof 23);
+//console.log(typeof "Jonas");
+
+javaScriptIsFun = "Yes";
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+*/
+
+/*let age = 30;
+age = 31;
+const birthyYear = 1991;
+birthyear = 1990;*/
+
+//Math Operators
+/*const now = 2037;
+const ageJonas = now - 1991;
+const ageSara = now - 2020;
+console.log(ageJonas, ageSara);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2**3 means 2 to the power of 3 = 2*2*2
+console.log(ageSara * 2, ageSara / 2 ** 3);
+
+const firstName = "Jonas";
+const lastName = "Schemedtmann";
+console.log(firstName + " " + lastName);*/
+
+//Assignment operator
+/*let x = 10 + 5;
+x += 10; //x =x+10=25
+x *= 4; // x =x*4=100
+x++; // x= x+1=101
+x--; // x=x-1
+x--;
+console.log(x);*/
+
+//Comparison operators
+/*console.log(ageJonas > ageSara);
+console.log(ageSara >= 18);
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSara) / 2;
+console.log(ageJonas, ageSara, averageAge);*/
+/*const marksMass = 78;
+const marksHeight = 1.69;
+const marksBMI = marksMass / marksHeight ** 2;
+const jhonsMass = 92;
+const jhonsHeight = 1.95;
+const jhonsBMI = jhonsMass / (jhonsHeight * jhonsHeight);
+const marksHigherBMI = marksBMI > jhonsBMI;
+
+console.log(marksBMI, jhonsBMI, marksHigherBMI);*/
+
+/*const marksMass = 95;
+const marksHeight = 1.88;
+const marksBMI = marksMass / marksHeight ** 2;
+const jhonsMass = 85;
+const jhonsHeight = 1.76;
+const jhonsBMI = jhonsMass / (jhonsHeight * jhonsHeight);
+const marksHigherBMI = marksBMI > jhonsBMI;
+
+console.log(marksBMI, jhonsBMI, marksHigherBMI);*/
+
+const firstName = "Zadid";
+const job = "code learner";
+const birthYear = 1991;
+const year = 2037;
+
+const Zadid =
+  "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+console.log(Zadid);
+
+const zadidNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(zadidNew);
+console.log(`just a regular string...`);
+console.log(`just
+a regular
+string...`);
